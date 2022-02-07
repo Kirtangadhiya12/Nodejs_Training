fa2d9152c11db1170709feaae4aa4b6ad4c2ab55
Kirtan Gadhiya <96760090+Kirtangadhiya12@users.noreply.github.com>
2022-02-07 18:23:07 +0530

    Add files via upload

---
+++ Scope/global_scope.js
@@ -0,0 +1,5 @@
+let satellite = 'The Moon', galaxy = 'The Milky Way', stars = 'North Star';
+function callMyNightSky(){
+  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
+}
+console.log(callMyNightSky());
