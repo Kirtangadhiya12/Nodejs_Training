51282cf348f9bc547690e1adee3d96581ea063cc
Kirtan Gadhiya <96760090+Kirtangadhiya12@users.noreply.github.com>
2022-02-17 14:50:46 +0530

    Delete Scope directory

--- Scope/global_scope.js
+++
@@ -1,5 +0,0 @@
-let satellite = 'The Moon', galaxy = 'The Milky Way', stars = 'North Star';
-function callMyNightSky(){
-  return 'Night Sky: ' + satellite + ', ' + stars + ', and ' + galaxy;
-}
-console.log(callMyNightSky());
