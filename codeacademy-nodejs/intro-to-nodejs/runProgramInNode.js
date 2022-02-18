79a76978341cb1c0343c5a0436f0d1347b7ffc38
Kirtan Gadhiya <96760090+Kirtangadhiya12@users.noreply.github.com>
2022-02-18 13:40:42 +0530

    added into to nodejs exercise file

---
+++ codeacademy-nodejs/intro-to-nodejs/runProgramInNode.js
@@ -0,0 +1,11 @@
+// running a program with node.
+let noun1 = 'cat';
+let adjective = 'silly';
+let noun2 = 'cat';
+let verb = 'jump';
+let noun3 = 'mouce';
+
+
+console.log(`The world's first ${noun1} was a very ${adjective} ${noun2} who loved to ${verb} while eating ${noun3} for every meal.`);
+
+//
