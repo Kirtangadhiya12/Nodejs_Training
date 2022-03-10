237b3308984cb315a638f2bec51ff016306e5ef5
Kirtan Gadhiya <96760090+Kirtangadhiya12@users.noreply.github.com>
2022-03-10 09:44:00 +0530

    A_day-1 Practice file added

---
+++ A-js-e-day-1/practi.js
@@ -0,0 +1,30 @@
+const xyz=[123,'js','hs'];
+//   const test=(a,b)=>{
+//     console.log( a+b);
+//   }
+ 
+     
+ 
+//  test(5,6);
+//  xyz.forEach((el)=>{
+//      console.log(`this is the ele ${el}`);
+     
+//     });
+    
+// const test = xyz.map((sl)=>{
+//     return sl;
+// })
+// console.log(test);
+// const exam=xyz.filter((kd)=>{
+//     return kd.length>1;
+// })
+// console.log(exam);
+const num=[1,2,3,4,5];
+const sum=num.reduce((accumulator,currentvalue)=>{
+    return accumulator+currentvalue;
+})
+console.log(sum);
+
+
+    
+ 
