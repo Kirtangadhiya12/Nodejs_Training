f87de424d4b02f4bb31c48b64926a6e3fcef562c
Kirtan Gadhiya <96760090+Kirtangadhiya12@users.noreply.github.com>
2022-03-11 15:06:22 +0530

    added Mongodb exercise file

---
+++ A-js-e-day-1/A-js-e-day-2/index.js
@@ -0,0 +1,16 @@
+const mySelf={
+    'name':'kirtan',
+    'age':22,
+    'degree':'bTech',
+    'collegeName':'BVM'
+}
+
+mySelf.address='anand';
+// console.log(mySelf);
+// objlength=Object.keys(mySelf).length;
+// console.log(objlength);
+objkey=Object.keys(mySelf);
+console.log(`this is a key of object ${objkey}`);
+objval=Object.values(mySelf);
+console.log(`this is the value of object ${objval}`);
+
