4a02991b176a7b5867a736f6136eab92c415fd43
Kirtan Gadhiya <96760090+Kirtangadhiya12@users.noreply.github.com>
2022-03-22 20:16:46 +0530

    Delete um.js

--- A-js-e-day-1/mongoose_CRUD_operations/um.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-const Schema = mongoose.Schema;
-const myUser=new Schema({
-    user_id:Number,
-    user_name:String,
-    user_email:String
-});
-module.exports=mongoose.model('user',myUser);
